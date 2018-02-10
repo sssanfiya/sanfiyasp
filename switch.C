e1479872a50982c616c3a33eb973dae4f02fea4e
sssanfiya <35294451+sssanfiya@users.noreply.github.com>
2018-02-10 13:48:12 +0530

    switch.c

---
+++ switch.C
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\n enter the number between 0-10:");
+scanf("%d",&a);
+switch(a)
+{
+case 1:
+printf("\n one");
+break;
+case 2:
+printf("\n two");
+break;
+case 3:
+printf("\n three");
+break;
+case 4:
+printf("\n four");
+break;
+case 5:
+printf("\n five");
+break;
+case 6:
+printf("\n six");
+break;
+case 7:
+printf("\n seven");
+break;
+case 8:
+printf("\n eight");
+break;
+case 9:
+printf("\n nine");
+break;
+case 10:
+printf("\n ten");
+break;
+default:
+printf("\n invalid choice");
+}
+return 0;
+}
