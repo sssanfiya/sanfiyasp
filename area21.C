cf0597dfbac6839380fd526badc343b13d32adca
sssanfiya <35294451+sssanfiya@users.noreply.github.com>
2018-02-21 15:38:47 +0530

    area21.c

---
+++ area21.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+double l,b,a;
+printf("\n enter the length of the farm:");
+scanf("%lf",&l);
+printf("\n enter the breadth of the farm:");
+scanf("%lf",&b);
+a=l*b;
+printf("the  area is:%lf",a);
+return 0;
+}
