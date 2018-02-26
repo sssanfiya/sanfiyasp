5f79fd7ba2e67c3f718cdb79e6b1d690f615757a
sssanfiya <35294451+sssanfiya@users.noreply.github.com>
2018-02-26 03:07:05 -0800

    string26.C

---
+++ strcat26.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char a[10],b[10];
+printf("\n enter the first string:");
+scanf("%s",a);
+printf("enter the second string:");
+scanf("%s",b);
+printf("%s%s",a,b);
+retrun 0;
+}
