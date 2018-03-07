e0c74ad5deb2b5a28162203e79f7be370c95f992
sssanfiya <35294451+sssanfiya@users.noreply.github.com>
2018-03-07 03:10:44 -0800

    sort24.c

---
+++ sort24.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int n,a[10],i,j,temp;
+printf('\n enter the no.of values:");
+scanf("%d",&n);
+printf("\n enter the values:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+  for(j=i+1;i<n;i++)
+  {
+   if(a[i]>a[j])
+   {
+   temp=a[i];
+   a[i]=a[j];
+   a[j]=temp;
+   }
+  }
+printf("\n %d",a[i]);
+}
+return 0;
+}
