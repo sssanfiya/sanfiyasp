c0c5356fcbffc7c1a7e7bb8cfc8b380ee2c16c06
sssanfiya <35294451+sssanfiya@users.noreply.github.com>
2018-03-07 02:14:24 -0800

    samll115.c

---
+++ small115.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n,a[10],i,k;
+printf("\n enter the no.of values and k value:");
+scanf("%d%d".&n.&k);
+printf("\n enter the values:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
