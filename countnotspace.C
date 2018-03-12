aa8e1189e78959d7b99bceb004cae8f7173cce91
sssanfiya <35294451+sssanfiya@users.noreply.github.com>
2018-03-12 16:17:53 +0530

    countnotsapce.C

---
+++ countnotspace.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10];
+int i,count=0;
+printf("\n enter the string:");
+gets(a);
+for(i=0;a[i]!='\0';i++)
+{
+  if(a[i]!=' ')
+  {
+  count++;
+  }
+}
+printf("%d",count);
+return 0;
+}
